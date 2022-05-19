24d9d9f7494b2198261dde135e8b5bf4bab6f7ef
jatindevnani <81103012+jatindevnani@users.noreply.github.com>
2022-05-19 09:42:59 +0530

    Made changes to and completed User.java file

--- E-Commerce/src/main/java/com/masai/Beans/User.java
+++ E-Commerce/src/main/java/com/masai/Beans/User.java
@@ -1,5 +1,37 @@
 package com.masai.Beans;
 
-public interface User {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="users")
+public abstract class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "user_name")
+	private String userName;
+	
+	@Column(name = "user_password")
+	private String userPassword;
+	
+	@Embedded
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name = "address_id")
+	private Address address;
 }
