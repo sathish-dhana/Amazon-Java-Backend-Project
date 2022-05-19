7e4fd38c48ddc99f4e64410f49c6d3c5eefc151d
Santhosh-1997 <99santhoshs@gmail.com>
2022-05-19 17:25:17 +0530

    productController added

--- E-Commerce/src/main/java/com/masai/controller/ProductController.java
+++ E-Commerce/src/main/java/com/masai/controller/ProductController.java
@@ -1,5 +1,31 @@
 package com.masai.controller;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.beans.Product;
+import com.masai.service.ProductServiceInterface;
+
+@RestController
+@RequestMapping("/ecommerce")
 public class ProductController {
 
+	@Autowired
+	private ProductServiceInterface productServ;
+	
+	@PostMapping("/product")
+	public ResponseEntity<Product> addProduct(@RequestBody Product product){
+		
+		Product productSaved = productServ.addProduct(product);
+		return new ResponseEntity<>(productSaved,HttpStatus.CREATED);
+	}
+	
 }
--- E-Commerce/src/main/java/com/masai/service/ProductService.java
+++ E-Commerce/src/main/java/com/masai/service/ProductService.java
@@ -1,8 +1,23 @@
 package com.masai.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.masai.beans.Product;
+import com.masai.repository.ProductCrudRepo;
 
 @Service
 public class ProductService implements ProductServiceInterface{
+
+	@Autowired
+	private ProductCrudRepo productRepo;
+	
+	@Override
+	public Product addProduct(Product product) {
+		
+		Product savedProduct = productRepo.save(product);
+		return savedProduct;
+		
+	}
 
 }
