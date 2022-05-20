357846ec00e1c505909f60a80ef422f1f82be43d
Santhosh-1997 <99santhoshs@gmail.com>
2022-05-20 11:54:47 +0530

    getProductbyId added

--- E-Commerce/src/main/java/com/masai/service/ProductServiceInterface.java
+++ E-Commerce/src/main/java/com/masai/service/ProductServiceInterface.java
@@ -1,9 +1,15 @@
 package com.masai.service;
 
+import java.util.List;
+
 import com.masai.beans.Product;
 
 public interface ProductServiceInterface {
 
 	public Product addProduct(Product product);
+
+	public List<Product> getAllProdcuts();
+
+	public Product getProductById(Integer id);
 
 }
