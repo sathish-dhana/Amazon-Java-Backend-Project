a6375acef256198b7bc1b36455c103695c202511
Santhosh-1997 <99santhoshs@gmail.com>
2022-05-20 19:34:05 +0530

    get product by category done

--- E-Commerce/src/main/java/com/masai/repository/ProductCrudRepo.java
+++ E-Commerce/src/main/java/com/masai/repository/ProductCrudRepo.java
@@ -1,11 +1,18 @@
 package com.masai.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.masai.beans.Product;
+import com.masai.beans.ProductCategory;
 
 @Repository
 public interface ProductCrudRepo extends JpaRepository<Product, Integer>{
 
+	
+	public List<Product> findByCategory(ProductCategory category);
+	
+	
 }
