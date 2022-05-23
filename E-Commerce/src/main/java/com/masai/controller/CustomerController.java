876ef9cffbcc26f3f8a741a3620296731345e898
jatindevnani <81103012+jatindevnani@users.noreply.github.com>
2022-05-23 09:43:16 +0530

    removed clutter

--- E-Commerce/src/main/java/com/masai/controller/CustomerController.java
+++ E-Commerce/src/main/java/com/masai/controller/CustomerController.java
@@ -101,9 +101,10 @@ public class CustomerController {
 		return new ResponseEntity(getCustomer, HttpStatus.CREATED);
 	}
 	
-	@PostMapping("/customer/addAddress/{customerId}")
-	public ResponseEntity<Customer> addCustomerAddress(@PathVariable("customerId") @Valid Integer customerId, @RequestBody Address address) {
-		Customer getCustomer = customerService.getCustomerById(customerId);
+	@PostMapping("/customer/addAddress")
+	public ResponseEntity<Customer> addCustomerAddress(@RequestParam String key, @RequestBody Address address) {
+		Login currentLogin = loginService.isTokenValid(key);
+		Customer getCustomer = customerService.getCustomerById(currentLogin.getUser().getUserId());
 		Address saveaddress = addressService.addAddress(address);
 		getCustomer.getAddresses().add(saveaddress);
 		return new ResponseEntity(getCustomer, HttpStatus.CREATED);
