bff3c1450fb8aa2dc4bc3e8c255a150b0ac2be86
sathish-dhana <kdhanasekar166@gmail.com>
2022-05-24 20:08:01 +0530

    removed unused methods

--- E-Commerce/src/main/java/com/masai/service/AddressServiceInterface.java
+++ E-Commerce/src/main/java/com/masai/service/AddressServiceInterface.java
@@ -10,14 +10,8 @@ import com.masai.beans.User;
 
 public interface AddressServiceInterface {
 	
-	//need to clarify doubt with team
-	
 	public Address addAddress(Address address);
-	public String removeAddressByAddressId(Integer addressId);
 	public String removeAllAddressOfUser(User user);
-	
-	
-	public Address updateAddress(Address address);
 	public Address showAddressById(Integer addressId);
 	
 	public Set<User> listAllUserByCity(String city);
